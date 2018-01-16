ec40c6a7f10623590e08924614a693079d0c98f9
dalia ramos <dalia.ramoshernandez@gmail.com>
2018-01-15 22:41:52 -0800

    add radio buttons

---
+++ README.md
@@ -0,0 +1,26 @@
+# _Independent Project 2_
+
+#### _{Brief description of application}, {Date of current version}_
+
+#### By Dalia Ramos
+
+## Description
+ Tells the user which track to join.
+## Setup/Installation Requirements
+
+* first created a file named project 2
+* secondly wrote how the webpage should look
+* lastly made radio buttons work by writing in javaScript.
+
+
+
+## Known Bugs
+none
+## Support and contact details
+if you run into any problems message creator by email Dalia.ramoshernandez@gmail.com
+## Technologies Used
+used javaScript
+
+
+
+Copyright (c) 2016 **_{Dalia Ramos}_**
---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $("button").click(function() {
+    $("#pageTwo").show();
+});
+
+  $("#test").submit(function(event){
+    var appAnswer = $("input:radio[name=app]:checked").val();
+    var easyHardAnswer = $("input:radio[name=easyHard]:checked").val();
+    var teachAnswer = $("input:radio[name=teach]:checked").val();
+    var teachAnswer = $("input:radio[name=teach]:checked").val();
+    var lengthAnswer = $("input:radio[name=length]:checked").val();
+    var slowOrFastAnswer = $("input:radio[name=slowOrFast]:checked").val();
+
+    if (appAnswer === "appJava" && easyHardAnswer ==="easyPython" && teachAnswer ==="teachPython" && lengthAnswer === "shortPython" && slowOrFastAnswer === "fastPython"){
+      var trackResults = "track 1"
+    }
+    else if (appAnswer === "appRuby" && easyHardAnswer ==="hardJava" && teachAnswer === "noTeachJava" && lengthAnswer === "longJava" && slowOrFastAnswer === "slowJava")
+
+    {
+      var trackresults = "track 2"
+    }
+    if (appAnswer && easyHardAnswer && teachAnswer && lengthAnswer && slowOrFastAnswer) {
+  $("#track").empty().append(trackResults);
+  $("#result").show();
+  }
