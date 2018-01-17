d9a8b3eec571ccd59a9d2e71d40bed449cb579bb
dalia ramos <dalia.ramoshernandez@gmail.com>
2018-01-16 21:44:21 -0800

    addsecond input.

--- js/scripts.js
+++ js/scripts.js
@@ -7,18 +7,16 @@ $(document).ready(function() {
     var appAnswer = $("input:radio[name=app]:checked").val();
     var easyHardAnswer = $("input:radio[name=easyHard]:checked").val();
     var teachAnswer = $("input:radio[name=teach]:checked").val();
-    var teachAnswer = $("input:radio[name=teach]:checked").val();
     var lengthAnswer = $("input:radio[name=length]:checked").val();
     var slowOrFastAnswer = $("input:radio[name=slowOrFast]:checked").val();
 
-    if (appAnswer === "appJava" && easyHardAnswer ==="easyPython" && teachAnswer ==="teachPython" && lengthAnswer === "shortPython" && slowOrFastAnswer === "fastPython"){
-      var trackResults = "track 1"
-    }
-    else if (appAnswer === "appRuby" && easyHardAnswer ==="hardJava" && teachAnswer === "noTeachJava" && lengthAnswer === "longJava" && slowOrFastAnswer === "slowJava"){
-      var trackResults = "track 2"
-    }
-    if (appAnswer && easyHardAnswer && teachAnswer && lengthAnswer && slowOrFastAnswer) {
-  $("#track").empty().append(trackResults);
+    if (appAnswer === "appJava" && easyHardAnswer === "easyPython" && teachAnswer === "teachPython" && lengthAnswer === "shortPython" && slowOrFastAnswer === "fastPython"){
+      var trackResult = "track 1"
+    } else {
+      var trackResult = "track 2"
+    }if (appAnswer && easyHardAnswer && teachAnswer && lengthAnswer && slowOrFastAnswer) {
+
+  $("#track").empty().append(trackResult);
   $("#result").show();
   }
   event.preventDefault();
